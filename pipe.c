f0fbee92af9bf3c73c4c40adb9b31bad084ea7ec
OksanaPugacheva <62735887+OksanaPugacheva@users.noreply.github.com>
2020-11-28 11:25:01 +0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -10,28 +10,29 @@ int main(){
     size_t size;
     char resstring[14]; 
     char ch; int j = 0;
+    int res;
 
     if(pipe(fd) < 0){
         printf("Не удалось создать канал\n");
         exit(-1); 
     } 
-
-    if(fork() < 0){ 
+res = fork();
+    if(res < 0){    
         printf("Не удалось создать подпроцесс\n");
         exit(-1);
 
-    } else if (fork() > 0) {
-        close(fd[0]);
-        size = write(fd[1], "Hello, world!", 14);
+    } else if (res> 0) {
+        close(fd[0]);       
+        size = write(fd[1], "Hello, world!", 14);  
         if(size != 14){
             printf("Не удалось прочитать стоку\n"); 
             exit(-1); 
         } 
-        close(fd[1]);
-        printf("Родитель закончил\n");
+        close(fd[1]);  
+        
 
     } else {
-        close(fd[1]);
+        close(fd[1]); 
         size = read(fd[0], resstring, 14);
         if(size < 0){
             printf("Не удалось записать строку\n"); 
